78be9eda1e3e48077d20fa41f78e87244406e031
Prakhar Varshney <77981004+prakharxvarshney@users.noreply.github.com>
2022-08-20 02:15:04 +0530

    Time: 11 ms (20.09%), Space: 48.5 MB (5.03%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.java
@@ -0,0 +1,28 @@
+class Solution {
+   public static String longestCommonPrefix(String[] strs) {
+        String str = new String();
+        int count = 0;
+        int min = Integer.MAX_VALUE;
+        //This loop for check the string of minimum length in the Array.
+        for (int i = 0; i < strs.length; i++) {
+            int len = strs[i].length();
+            if (len < min) {
+                min = len;
+                str = strs[i];
+            }
+        }
+
+        for(int i=0;i<min;i++) {
+            int c = 0;
+            for(int j=0;j<strs.length;j++) {
+                char[] ch = strs[j].toCharArray();
+                if(str.charAt(i)!=ch[i]) {
+                    return str.substring(0,count);
+                }
+            }
+            count++;
+        }
+        return str.substring(0,count);
+
+    }
+}
