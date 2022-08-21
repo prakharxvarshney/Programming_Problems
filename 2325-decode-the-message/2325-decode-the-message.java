cebfd7d6a5184e5d4bfa9510211f02e22dba62fc
Prakhar Varshney <77981004+prakharxvarshney@users.noreply.github.com>
2022-08-21 23:34:33 +0530

    Time: 23 ms (17.17%), Space: 54.1 MB (17.75%) - LeetHub

--- 2325-decode-the-message/2325-decode-the-message.java
+++ 2325-decode-the-message/2325-decode-the-message.java
@@ -1,33 +1,39 @@
 class Solution {
     public static String decodeMessage(String key, String message) {
         HashMap<Character,Character> map = new HashMap<>();
-        char[] ch = new char[26];
-        int c=97;
-        int j = 0;
-        for(int i=0;i<key.length();i++) {
-            if(key.charAt(i)!=(char)32) {
-                if(!map.containsValue(key.charAt(i))) {
-                    map.put((char)c,key.charAt(i));
-                    c+=1;
-                    ch[j] = key.charAt(i);
-                    j++;
-                }
+
+        int n = key.length();
+
+        char c  = 'a';
+
+        for(int i=0;i<n;i++)
+        {
+            char ch = key.charAt(i);
+            if(ch==' ')
+            {
+                continue;
+            }
+            if(!map.containsKey(ch))
+            {
+                map.put(ch,c);
+                c = (char)(c+1);
             }
         }
+
         String ans = "";
 
-        for (int i=0;i<message.length();i++) {
-            if(message.charAt(i)==(char)32) {
-                ans+=message.charAt(i);
+        int m = message.length();
+
+        for(int i=0;i<m;i++)
+        {
+            char ch = message.charAt(i);
+
+            if(ch==' ')
+            {
+                ans += " ";
             }
-            else {
-                for(int k =0;k<ch.length;k++) {
-                    if(message.charAt(i)==ch[k]) {
-                        int a=k+97;
-                        ans+=(char)a;
-                    }
-                }
-            }
+            else
+                ans += (map.get(ch));
         }
 
         return ans;
