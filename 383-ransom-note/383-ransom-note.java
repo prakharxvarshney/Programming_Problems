9d9fd4de45f054be436c5976cec9729248c06242
Prakhar Varshney <77981004+prakharxvarshney@users.noreply.github.com>
2022-08-25 11:01:56 +0530

    Time: 38 ms (20.02%), Space: 52 MB (30.97%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.java
@@ -0,0 +1,32 @@
+class Solution {
+    public static boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character,Integer> map  = new HashMap<>();
+        for(int i=0;i<magazine.length();i++) {
+            char ch = magazine.charAt(i);
+            if(!map.containsKey(ch)) {
+                map.put(ch,1);
+            }
+            else {
+                int n = map.get(ch);
+                map.put(ch,n+1);
+            }
+        }
+        System.out.println(map);
+
+        int c= 0;
+        for(int i=0;i<ransomNote.length();i++) {
+            char ch = ransomNote.charAt(i);
+            if (map.containsKey(ch) && map.get(ch)>0) {
+                int n = map.get(ch);
+                c++;
+                map.put(ch, n - 1);
+            } else {
+                break;
+            }
+        }
+        if(c==ransomNote.length()) {
+            return true;
+        }
+        else return false;
+    }
+}
