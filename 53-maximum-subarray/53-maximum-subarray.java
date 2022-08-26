90b30d3d4d4a235d052b0921029db52606a77079
Prakhar Varshney <77981004+prakharxvarshney@users.noreply.github.com>
2022-08-26 19:06:18 +0530

    Time: 1 ms (100.00%), Space: 73.2 MB (72.56%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -4,12 +4,9 @@ class Solution {
         int max=0;
         for (int i=0;i<nums.length;i++) {
             max+=nums[i];
-            if(min<max) {
-                min=max;
-            }
+            if(min<max) min=max;
             if(max<0) max=0;
         }
-
         return min;
     }
 }
